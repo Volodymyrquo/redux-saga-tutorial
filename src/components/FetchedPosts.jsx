b304478d3d18083b9b144e40823abfee7a7256aa
Volodymyr <volodymyrquo@gmail.com>
2021-02-27 17:31:39 +0200

    added loader

--- src/components/FetchedPosts.jsx
+++ src/components/FetchedPosts.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchPosts } from '../redux/actions'
 import Post from './Post'
 
-const FetchedPosts = ({posts}) => {
+const FetchedPosts = () => {
      const dispatch = useDispatch()
-    if(!posts.length) return <button
+     const posts = useSelector(state => state.posts.fetchedPosts)
+     const loading = useSelector(state => state.app.loading)
+
+     if(loading) {
+         return <div className="spinner-border text-success" role="status">
+         <span className="sr-only">Loading...</span>
+       </div>
+     }
+    if(!posts.length){ return <button
      className="btn btn-primary"
      onClick={()=>dispatch(fetchPosts())}
-     >Fetched</button>
+     >Fetched</button>}
 
-    return posts.map((post,idx) => <Post post={post} key={idx} />)
+    return posts.map((post) => <Post post={post} key={post.id} />)
 }
 
 export default FetchedPosts
